cf8ade1293317e4cd45c566e012e7168ba0c122c
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2019-09-24 17:07:16 -0500

    Ejemplo de escaner QR

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Solo funciona con https</p>
+    <video width="500"></video>
+    <div>Contenido: {{result}}</div>
+  </div>
+</template>
+
+<script>
+import QrScanner from "qr-scanner";
+
+export default {
+  name: "App",
+  data: () => {
+    return {
+      result: ""
+    };
+  },
+  mounted() {
+    const qrScanner = new QrScanner(this.$el.children[1], result => {
+      this.result = result;
+    });
+    qrScanner.start();
+  }
+};
+</script>
+
+<style>
+</style>
