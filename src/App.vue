7db3bcd378ae581ecca8357f486e58f8a1d6bd24
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2019-09-24 18:01:52 -0500

    Use jsQR

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <p>Solo funciona con https</p>
-    <video width="500"></video>
+    <canvas width="500" height="400"></canvas>
+    <video></video>
     <div>Contenido: {{result}}</div>
   </div>
 </template>
 
 <script>
-import QrScanner from "qr-scanner";
+import jsQR from "jsqr";
 
 export default {
   name: "App",
@@ -17,13 +17,49 @@ export default {
     };
   },
   mounted() {
-    const qrScanner = new QrScanner(this.$el.children[1], result => {
-      this.result = result;
-    });
-    qrScanner.start();
+    var video = this.$el.children[1];
+    var canvasElement = this.$el.children[0];
+    var canvas = canvasElement.getContext("2d");
+
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: "environment" } })
+      .then(function(stream) {
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true);
+        video.play();
+        requestAnimationFrame(tick);
+      });
+
+    const tick = () => {
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvas.drawImage(
+          video,
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+        );
+        var imageData = canvas.getImageData(
+          0,
+          0,
+          canvasElement.width,
+          canvasElement.height
+        );
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+          inversionAttempts: "dontInvert"
+        });
+        if (code) {
+          this.result = code.data;
+        }
+      }
+      requestAnimationFrame(tick);
+    };
   }
 };
 </script>
 
 <style>
+video {
+  display: none;
+}
 </style>
